4c92a0017b11fa04e187f78a4af4c2fce6765271
Aldrin Martoq <a@a0.cl>
2018-05-28 17:16:32 -0400

    Adding reload! for debugging in bin/console

---
+++ lib/a0/reload.rb
@@ -0,0 +1,5 @@
+def reload!(base = 'a0-tzmigration-ruby')
+  $LOADED_FEATURES.select { |feature| feature =~ %r{\/#{base}\/lib\/} }.each do |feature|
+    puts "Reloading #{feature.gsub(/.*#{base}/, base)}: #{load feature}"
+  end
+end
