1e0901bf0aab27d78be8975ed45d042da91edcbd
Aldrin Martoq <a@a0.cl>
2018-06-03 00:48:24 -0400

    Fixing bug 'git' not found when doing a require 'a0-tzmigration-ruby'.

--- lib/a0/tzmigration.rb
+++ lib/a0/tzmigration.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require 'a0/tzmigration/config'
-require 'a0/tzmigration/data_generator'
 require 'a0/tzmigration/tzversion'
 require 'a0/tzmigration/util'
 require 'a0/tzmigration/version'
--- lib/a0/tzmigration/util.rb
+++ lib/a0/tzmigration/util.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'rest-client'
+require 'json'
 
 module A0
   module TZMigration
