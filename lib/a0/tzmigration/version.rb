bd40b9ba0057048f9bc6147142ad6b77628bf508
Aldrin Martoq <a@a0.cl>
2018-06-04 18:06:31 -0400

    v1.0.2

--- lib/a0/tzmigration/version.rb
+++ lib/a0/tzmigration/version.rb
@@ -2,6 +2,6 @@
 
 module A0
   module TZMigration
-    VERSION = '1.0.1'
+    VERSION = '1.0.2'
   end
 end
--- spec/a0/tzmigration_spec.rb
+++ spec/a0/tzmigration_spec.rb
@@ -3,6 +3,6 @@
 RSpec.describe A0::TZMigration do
   it 'has a version number' do
     expect(A0::TZMigration::VERSION).not_to be nil
-    expect(A0::TZMigration::VERSION).to eq('1.0.1')
+    expect(A0::TZMigration::VERSION).to eq('1.0.2')
   end
 end
