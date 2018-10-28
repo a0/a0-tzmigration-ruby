223374e5efb3406a3afb3ae985ab70f4b85ce18e
Aldrin Martoq <a@a0.cl>
2018-10-28 01:14:58 -0300

    Rubocop compliance.

--- lib/a0/reload.rb
+++ lib/a0/reload.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 def reload!(base = 'a0-tzmigration-ruby')
   $LOADED_FEATURES.select { |feature| feature =~ %r{\/#{base}\/lib\/} }.each do |feature|
     puts "Reloading #{feature.gsub(/.*#{base}/, base)}: #{load feature}"
